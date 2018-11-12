f23793c4fa8156725cf8163b67b55e40ca9487cf
Collin M <wingedillidan@gmail.com>
2018-11-11 19:49:22 -0500

    It didn't work travis

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -3,19 +3,75 @@ import * as React from 'react';
 // Stylings
 import './Intro.scss';
 
+const TRANSITION_CLASSNAME = 'active';
+
 export default class Intro extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            horizontals: this.generateSpans(26),
+            transitionClass: TRANSITION_CLASSNAME,
+            poop: 0
+        }
+
+        this.bump = this.bump.bind(this);
+    }
+    componentDidMount() {
+        setTimeout(this.bump, 2000);
+    }
+    generateSpans(n, poopSetter=0) {
+        const result = [];
+
+        for (let i = 0; i < n; i++) {
+            if (i === poopSetter) {
+                result.push(<span POOP="1" />);
+            } else {
+                result.push(<span />);
+            }
+        }
+
+        return result;
+    }
+    bump () {
+        const {horizontals, poop} = this.state;
+        const enterFrom = horizontals.slice(1);
+        const enterTo = horizontals.slice();
+
+        // this.setState({transitionClass: '', horizontals: enterFrom}, () => {
+        //     this.setState({transitionClass: TRANSITION_CLASSNAME, horizontals: enterTo});
+        //     setTimeout(this.bump, 2000);
+        // });
+
+        this.setState({poop: poop + 1}, () => {
+            this.setState({horizontals: this.generateSpans(25, this.state.poop)})
+            setTimeout(this.bump, 2000);
+        });
+
+        // this.setState({transitionClass: '', horizontals: enterFrom});
+        // this.forceUpdate(() => {
+        //     this.setState({transitionClass: TRANSITION_CLASSNAME, horizontals: enterTo});
+        //     setTimeout(this.bump, 2000);
+        // })
+
+    }
     render () {
+        const {horizontals, transitionClass} = this.state;
         return (
             <div className="backdrop">
-                <p>HELLO</p>
                 <div className="horizon">
-                    <span id="verticals-left-1" />
-                    <span id="verticals-left-2" />
-                    <span id="horizontals-1" />
-                    <span id="horizontals-2" />
+                    <div className="verticals-left">
+                        {this.generateSpans(10)}
+                    </div>
+                    <div className="verticals-center">
+                        <span />
+                    </div>
+                    <div className="verticals-right">
+                        {this.generateSpans(10)}
+                    </div>
+                    <div className={`horizontals ${transitionClass}`}>
+                        {horizontals}
+                    </div>
                 </div>
             </div>
         )
