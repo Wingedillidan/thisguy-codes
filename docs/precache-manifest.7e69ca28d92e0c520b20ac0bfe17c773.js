06ae550c5fea556877ecd55b76dcf188f9bb806c
Collin M <wingedillidan@gmail.com>
2018-11-12 20:40:03 -0500

    Prepare build for github pages

---
+++ docs/precache-manifest.7e69ca28d92e0c520b20ac0bfe17c773.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a5683e286e998579b89e",
+    "url": "/static/js/main.a5683e28.chunk.js"
+  },
+  {
+    "revision": "0b13b4885e8ccff8ae99",
+    "url": "/static/js/1.0b13b488.chunk.js"
+  },
+  {
+    "revision": "a5683e286e998579b89e",
+    "url": "/static/css/main.f7a2ef8b.chunk.css"
+  },
+  {
+    "revision": "7619beae03d4958d63dffe8bcaaf8d02",
+    "url": "/index.html"
+  }
+];
